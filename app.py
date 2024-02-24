90e14bf7fb23b95297f7daff712c086da53e6a57
donitanhero <doni.tan55@gmail.com>
2024-02-24 10:36:15 +0700

    24/2/2024
    - typo correction

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import mysql.connector
-import nltk
 from nltk.tokenize import MWETokenizer
+from fuzzywuzzy import process
+import numpy as np
 
 app = Flask(__name__)
 
@@ -26,7 +27,22 @@ multi_word_expressions = [
     ('jakarta', 'pusat')
 ]
 
-tokenizer = MWETokenizer(multi_word_expressions, separator=' ')
+#multi_word_expressions = []
+
+_jenistRumahSakit = []
+_kelurahan = []
+_kecamatan =[]
+_kotaAdministrasi = []
+
+
+'''allowed_keywords = [
+    'jakarta', 'selatan', 'timur', 'barat', 'pusat', 'utara',
+    'Bandung', 'Surabaya', 'Cilandak Barat', 'Kebon Jeruk', 'Cengkareng', 'Grogol', 'sakit'
+]'''
+
+allowed_keywords = []
+
+
 
 
 # Fungsi untuk mengambil data dari MySQL
@@ -51,7 +67,50 @@ def load_data_from_mysql():
         # Menggabungkan kolom relevan untuk pencarian
         df['search_data'] = df.apply(lambda row: f"{row['kode_rumah_sakit']} {row['nama_rumah_sakit']} {row['jenis_rumah_sakit']} {row['alamat_rumah_sakit']} {row['kelurahan']} {row['kecamatan']} {row['kota_administrasi']} {row['kode_pos']} {row['nomor_telepon']} {row['nomor_fax']} {row['website']} {row['email']} {row['telepon_humas']} {row['website']}", axis=1)
 
-        return df
+        allowedWordList = []
+
+        cursor.execute("SELECT DISTINCT jenis_rumah_sakit FROM `data_rumah_sakit_di_dki_jakarta` WHERE jenis_rumah_sakit != '-'")
+        rows = cursor.fetchall()
+
+        _jenistRumahSakit  = set(pd.DataFrame(rows)['jenis_rumah_sakit'].tolist()) 
+        allowedWordList.append([word.lower() for string in _jenistRumahSakit for word in string.split()])
+        allowedWordList.append(pd.DataFrame(rows)['jenis_rumah_sakit'].str.lower().tolist())
+        
+        _jenistRumahSakit = [tuple(s.lower().split(" ")) for s in _jenistRumahSakit ] 
+
+
+        cursor.execute("SELECT DISTINCT kota_administrasi FROM `data_rumah_sakit_di_dki_jakarta` WHERE kota_administrasi != '-'")
+        rows = cursor.fetchall()
+
+        _kotaAdministrasi  = set(pd.DataFrame(rows)['kota_administrasi'].tolist()) 
+        allowedWordList.append([word.lower() for string in _kotaAdministrasi for word in string.split()])
+        allowedWordList.append(pd.DataFrame(rows)['kota_administrasi'].str.lower().tolist())
+        
+        _kotaAdministrasi = [tuple(s.lower().split(" ")) for s in _kotaAdministrasi ] 
+        
+        
+        cursor.execute("SELECT DISTINCT kelurahan FROM `data_rumah_sakit_di_dki_jakarta` WHERE kelurahan != '-'")
+        rows = cursor.fetchall()
+
+        _kelurahan = set(pd.DataFrame(rows)['kelurahan'].tolist()) 
+        allowedWordList.append([word.lower() for string in _kelurahan for word in string.split()])
+        _kelurahan = [tuple(s.lower().split(" ")) for s in _kelurahan]
+        
+    
+
+        cursor.execute("SELECT DISTINCT kecamatan FROM `data_rumah_sakit_di_dki_jakarta` WHERE kecamatan != '-'")
+        rows = cursor.fetchall()
+
+        _kecamatan = set(pd.DataFrame(rows)['kecamatan'].tolist()) 
+        allowedWordList.append([word.lower() for string in _kecamatan for word in string.split()])
+        _kecamatan = [tuple(s.lower().split(" ")) for s in _kecamatan]
+        
+
+        allowed_keywords = [item for sublist in allowedWordList for item in sublist]
+        allowed_keywords.append('rumah')
+        allowed_keywords.append('sakit')
+
+        return df, _kotaAdministrasi, _kelurahan, _kecamatan, allowed_keywords, _jenistRumahSakit
 
     except mysql.connector.Error as err:
         print(f"Error: {err}")
@@ -63,35 +122,62 @@ def load_data_from_mysql():
             conn.close()
 
 # Inisialisasi dan fit TF-IDF Vectorizer
-df = load_data_from_mysql()
+df, _kotaAdministrasi, _kelurahan, _kecamatan, allowed_keywords, _jenistRumahSakit = load_data_from_mysql()
 if not df.empty:
     tfidf_vectorizer = TfidfVectorizer()
     tfidf_matrix = tfidf_vectorizer.fit_transform(df['search_data'])
+
+#tokenizer = MWETokenizer(multi_word_expressions, separator=' ')
+kota_tokenizer = MWETokenizer(_kotaAdministrasi, separator=' ')
+#kelurahan_tokenizer = MWETokenizer(_kelurahan, separator=' ')
+#kecamatan_tokenizer = MWETokenizer(_kecamatan, separator=' ')
+
+
+
+def correct_typos(query, allowed, limit=1):
+    corrected_query = []
+    for word in query.split():
+        # Cari kata yang paling mirip, dengan skor minimal 80
+        result = process.extractOne(word, allowed_keywords, score_cutoff=50)
+        if result:  # Cek apakah ada hasil yang ditemukan
+            corrected_word, score = result  # Unpacking karena result bukan None
+            corrected_query.append(corrected_word)
+        else:
+            corrected_query.append(word)  # Jika tidak ada yang cukup mirip, gunakan kata asli
+    
+    return ' '.join(corrected_query)
 
 @app.route('/', methods=['GET', 'POST'])
 def search():
     results = []
     if request.method == 'POST' and not df.empty:
-        query = request.form['query'].lower()
+        original_query = request.form['query']
+        corrected_query = correct_typos(original_query, allowed_keywords)  # Koreksi typo
         
-        # Tokenisasi query dengan mempertimbangkan multi-word expressions
-        tokens = tokenizer.tokenize(query.split())
+        kota_tokens = kota_tokenizer.tokenize(corrected_query.split())  # Gunakan query yang sudah dikoreksi
+  
+        kota = [token for token in kota_tokens if token.lower() in [keyword.lower() for keyword in [' '.join(tup) for tup in _kotaAdministrasi]]]
+
         
-        # Asumsikan lokasi spesifik dapat dikenali sebagai satu token
-        locations = [token for token in tokens if token in ['jakarta utara', 'jakarta selatan', 'jakarta barat', 'jakarta timur', 'jakarta pusat']]
-        
-        query_vector = tfidf_vectorizer.transform([' '.join(tokens)])  # Gunakan tokens yang sudah ditokenisasi
+
+        query_vector = tfidf_vectorizer.transform([corrected_query])  # Gunakan query yang sudah dikoreksi
         similarities = cosine_similarity(query_vector, tfidf_matrix).flatten()
         top_indices = similarities.argsort()[::-1]
-         # Filter berdasarkan nilai kesamaan > 0.1
+        
         filtered_indices = [i for i in top_indices if similarities[i] > 0.1]
 
-        if locations:  # Jika ada kata kunci lokasi yang dikenali, filter hasil berdasarkan kata kunci
-            results = df.iloc[filtered_indices][df['kota_administrasi'].str.contains('|'.join(locations), case=False, na=False)]
-        else:  # Jika tidak ada kata kunci lokasi, gunakan skor TF-IDF yang telah difilter
-            results = df.iloc[filtered_indices]
+        
 
-        return render_template('index.html', results=results.to_dict('records'), query=query)
+        if kota:
+            results = df.iloc[filtered_indices][df['kota_administrasi'].str.contains('|'.join(kota), case=False, na=False)]
+        else:
+            results = df.iloc[filtered_indices]
+      
+
+
+
+
+        return render_template('index.html', results=results.to_dict('records'), query=original_query, corrected_query=corrected_query)
     return render_template('index.html', results=None)
 
 if __name__ == '__main__':
